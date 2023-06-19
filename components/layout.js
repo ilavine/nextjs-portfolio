d92c9a0409b01e2cd05d8b1ca5a64ab5368e45da
unknown <iuliia.lavine@gmail.com>
2023-06-19 14:15:48 -0400

    fix alignment on mobile

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
-import Link from "next/link";
 import Footer from "../components/footer";
 import Navbar from "../components/sidebar";
+import styles from "../styles/utils.module.css";
 
 export const siteTitle = "Next.js Portfolio - Yulia Lavine";
 
@@ -35,46 +35,50 @@ export default function Layout({ children }) {
     };
   }, []);
 
-
-
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
-    <><>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Yulia Lavine - Web Developer Portfolio" />
-      </Head>
-      <div className="flex min-h-screen flex-col md:flex-row bg-gray-100 text-gray-800">
-        <aside
-          className={`sidebar w-full md:w-64 bg-white p-4 transition-transform duration-150 ease-in md:translate-y-0 md:shadow md:mr-4`}
-        >
- <div className="my-4 md:mt-24 w-full border-b-4 border-gray-100 text-center">            <span className="font-mono text-3xl font-bold tracking-widest">
-              YL
-            </span>
-          </div>
-          < Navbar />
-
- 
-        </aside>
-        <main className="main flex-grow flex p-4 transition-all duration-150 ease-in flex justify-center">
-          {children}
-        </main>
-        
-        {showArrowUp && (
-          <button
-            className="fixed bottom-6 right-6 rounded-full p-2 border-2 border-gray-800"
-            onClick={scrollToTop}
-            aria-label="Scroll to Top"
+    <>
+      <>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Yulia Lavine - Web Developer Portfolio"
+          />
+        </Head>
+        <div className="flex min-h-screen flex-col md:flex-row bg-gray-100 text-gray-800 overflow-hidden">
+          <aside
+            className={`sidebar w-full md:w-64 bg-white p-4 transition-transform duration-150 ease-in md:translate-y-0 md:shadow md:mr-4`}
           >
-            &#8593;
-          </button>
-        )}
-      </div>
-    </><Footer /></>
+            <div className="my-4 md:mt-24 w-full border-b-4 border-gray-100 text-center">
+              {" "}
+              <span className="font-mono text-3xl font-bold tracking-widest">
+                YL
+              </span>
+            </div>
+            <Navbar />
+          </aside>
+          <main
+            className={`main flex-grow flex ${styles.center} p-4 transition-all duration-150 ease-in overflow-hidden`}
+          >
+            {children}
+          </main>
+
+          {showArrowUp && (
+            <button
+              className="fixed bottom-6 right-6 rounded-full p-2 border-2 border-gray-800"
+              onClick={scrollToTop}
+              aria-label="Scroll to Top"
+            >
+              &#8593;
+            </button>
+          )}
+        </div>
+      </>
+      <Footer />
+    </>
   );
 }
